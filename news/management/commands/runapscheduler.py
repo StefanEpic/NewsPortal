70ffc3944a402cd23ee2e538767fd9024f165fd4
StefanEpic <epiktetov.sv@gmail.com>
2023-03-31 12:00:49 +0300

    add subscribe

--- news/management/commands/runapscheduler.py
+++ news/management/commands/runapscheduler.py
@@ -1,22 +1,44 @@
+import datetime
 import logging
 
 from django.conf import settings
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
+from django.core.mail import EmailMultiAlternatives
 from django.core.management.base import BaseCommand
+from django.template.loader import render_to_string
 from django_apscheduler.jobstores import DjangoJobStore
 from django_apscheduler.models import DjangoJobExecution
 
+from news.models import Post, Category
 
 logger = logging.getLogger(__name__)
 
 
 
 def my_job():
-    print('hello from job')
+    today = datetime.datetime.now()
+    last_week = today - datetime.timedelta(days=7)
+    posts = Post.objects.filter(date_in__gte=last_week)
+    categories = set(posts.values_list('category__theme', flat=True))
+    subscribers = set(Category.objects.filter(theme__in=categories).values_list('subscribers__email', flat=True))
+    html_content = render_to_string(
+        'daily_post.html',
+        {
+            'link': settings.SITE_URL,
+            'posts': posts,
+        }
+    )
 
-
+    msg = EmailMultiAlternatives(
+        subject='Статьи за неделю',
+        body='',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=subscribers,
+    )
+    msg.attach_alternative(html_content, 'text/html')
+    msg.send()
 
 def delete_old_job_executions(max_age=604_800):
     """This job deletes all apscheduler job executions older than `max_age` from the database."""
@@ -32,7 +54,7 @@ class Command(BaseCommand):
 
         scheduler.add_job(
             my_job,
-            trigger=CronTrigger(second="*/10"),
+            trigger=CronTrigger(day_of_week="mon", hour="00", minute="00"),
             id="my_job",
             max_instances=1,
             replace_existing=True,
@@ -43,7 +65,7 @@ class Command(BaseCommand):
             delete_old_job_executions,
             trigger=CronTrigger(
                 day_of_week="mon", hour="00", minute="00"
-            ), 
+            ),
             id="delete_old_job_executions",
             max_instances=1,
             replace_existing=True,
